4e09062b7a91985e3661901b0cfe91eefa667e23
kuma-1220 <sato.kuma.t7@dc.tohoku.ac.jp>
2025-07-25 08:20:24 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@mantine/core/styles.css"
+import { MantineProvider } from "@mantine/core"
+
+function App() {
+  return (
+    <MantineProvider>
+      this is my test app
+    </MantineProvider>
+  )
+}
+
+export default App
