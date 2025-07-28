4aff68087db3d055f7993aafcd678a748b12517a
Kuma <sato.kuma.t7@dc.tohoku.ac.jp>
2025-07-28 19:21:50 +0900

    style: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import "@mantine/core/styles.css"
-import { MantineProvider } from "@mantine/core"
+
 
 function App() {
   return (
-    <MantineProvider>
+    <>
       this is my test app
-    </MantineProvider>
+    </>
   )
 }
 
